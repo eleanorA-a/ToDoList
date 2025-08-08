63711880f6dfc3cf7963626523eb6c1241369ef1
Eleanor Abitbol <eleanor.abitbol@gmail.com>
2025-08-08 13:10:10 -0400

    added look alike plus button and also title

---
+++ NewToDoView.swift
@@ -0,0 +1,20 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        VStack{
+            Text(")
+        }//end of Vstack
+    }//end of body
+}//end of struct
+
+#Preview {
+    NewToDoView()
+}
